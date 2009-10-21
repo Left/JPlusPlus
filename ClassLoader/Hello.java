46e493ae61659f4608a9ad538b8229ba2f9edf05
unknown <vadim.ridosh@.teamdev.local>
2009-10-21 14:24:35 +0300

    TheCommit

--- ClassLoader/Hello.java
+++ ClassLoader/Hello.java
@@ -3,6 +3,11 @@ public class Hello
    // Comment from me!
    // Another comment from me!
    // Third comment from me!
+   
+
+	/**
+	*
+	**/
    public static void main( String[] args )
    {
       System.out.println( "Hello world" );

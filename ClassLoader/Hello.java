8df53f71fe440c821113dd13fb3b6afa27fdcf26
unknown <vadim.ridosh@.teamdev.local>
2009-10-21 14:12:42 +0300

    Third comment

--- ClassLoader/Hello.java
+++ ClassLoader/Hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
    // Comment from me!
    // Another comment from me!
+   // Third comment from me!
    public static void main( String[] args )
    {
       System.out.println( "Hello world" );
